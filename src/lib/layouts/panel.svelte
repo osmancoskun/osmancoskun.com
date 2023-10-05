104c20b75a1446173a8e77244cff3dc28eae5275
Osman Coskun <osman.coskun95@gmail.com>
2023-10-05 21:00:42 +0300

    feat: panel layout designed

---
+++ src/lib/layouts/panel.svelte
@@ -0,0 +1,32 @@
+<div class="panel">
+    <span class="activity">Activities</span>
+    <span>Oct 5 19:53</span>
+    <span class="panel-settings">
+        <span>en</span>
+        <span>pc icon</span>
+        <span>sound icon</span>
+        <span>power icon</span>
+    </span>
+</div>
+
+<style>
+    .panel {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        height: var(--panel-height);
+        width: 100%;
+        background: var(--panel-color);
+        color: white;
+    }
+    .activity {
+        margin-left: 20px;
+    }
+    .panel-settings {
+        margin-right: 20px;
+    }
+    span {
+        margin: 0;
+        padding: 0;
+    }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,7 @@
+<script>
+	import "./styles.css";
+	import Layout from "$lib/layouts/panel.svelte";
+</script>
+
+<Layout />
 <slot />
